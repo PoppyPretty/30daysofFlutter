5d6eda02b795a9dd3d4337f1e451c7ab15e9387a
Paul John Thomas <pm.xlri@gmail.com>
2023-08-25 15:50:19 +0530

    themes extracted

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:poppy_catalog/screens/home_page.dart';
 import 'package:poppy_catalog/screens/login_page.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:poppy_catalog/utils/routes.dart';
+import 'package:poppy_catalog/widgets/themes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,13 +15,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
       initialRoute: "/",
       routes: {
