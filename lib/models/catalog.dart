8b15d0d40387a1f833d9320341cc6c287d8033df
Paul John Thomas <pm.xlri@gmail.com>
2023-08-29 07:21:41 +0530

    Day 12 and 13 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,27 +1,29 @@
+class CatalogModel {
+  static final items = [
+    Item(
+        id: 1,
+        name: "iPhone 12 Pro",
+        desc: "Apple iPhone 12th generation",
+        price: 999,
+        color: "#33505a",
+        image: "https://images.unsplash.com/photo-1589492477829-5e65395b66cc")
+  ];
+}
+
 class Item {
-  late final String id;
+  late final int id;
   late final String name;
   late final String desc;
+  late final num price;
   late final String color;
   late final String image;
-  late final num price;
 
   Item({
     required this.id,
     required this.name,
     required this.desc,
+    required this.price,
     required this.color,
     required this.image,
-    required this.price,
   });
-
-  final products = [
-    Item(
-        id: "Codepur001",
-        name: "iPhone 12 Pro",
-        desc: "Apple iPhone 12th generation",
-        color: "#33505a",
-        image: "https://images.unsplash.com/photo-1589492477829-5e65395b66cc",
-        price: 999)
-  ];
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,21 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'package:poppy_catalog/models/catalog.dart';
 import 'package:poppy_catalog/widgets/drawer.dart';
+import 'package:poppy_catalog/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJSON =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJSON);
+    var productData = decodedData["products"];
+    print("productsData");
+  }
+
+  @override
   Widget build(BuildContext context) {
-    const days = 30;
-    const name = "Codepur";
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
 
     return Scaffold(
       appBar: AppBar(
         title: const Text("Poppy's Catalog"),
       ),
-      body: const Center(
-        child:
-            Text("Hello World! \n\n Welcome to $days days of Flutter by $name"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: ((context, index) {
+            return ItemWidget(item: dummyList[index]);
+          }),
+        ),
       ),
       drawer: const MyDrawer(),
     );
