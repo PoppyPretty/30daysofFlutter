35a3710d5d0d5c517d99bdf0f5eaeda444b5c653
Paul John Thomas <pm.xlri@gmail.com>
2023-08-31 14:55:36 +0530

    Day 12 to 15 completed

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -20,29 +20,36 @@ class _HomePageState extends State<HomePage> {
   }
 
   loadData() async {
+    // await Future.delayed(const Duration(seconds: 2));
     final catalogJSON =
         await rootBundle.loadString("assets/files/catalog.json");
     final decodedData = jsonDecode(catalogJSON);
-    var productData = decodedData["products"];
-    print("productsData");
+    var productsData = decodedData["products"];
+
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text("Poppy's Catalog"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: dummyList.length,
-          itemBuilder: ((context, index) {
-            return ItemWidget(item: dummyList[index]);
-          }),
-        ),
+        child: (CatalogModel.items.isNotEmpty)
+            ? ListView.builder(
+                itemCount: CatalogModel.items.length,
+                itemBuilder: (context, index) => ItemWidget(
+                  item: CatalogModel.items[index],
+                ),
+              )
+            : const Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: const MyDrawer(),
     );
