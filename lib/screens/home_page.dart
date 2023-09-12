0e9e27a36e3da248e1158391905897ac11a30d9a
Paul John Thomas <pm.xlri@gmail.com>
2023-09-12 11:32:51 +0530

    Day 16 completed

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -41,11 +41,19 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items.isNotEmpty)
-            ? ListView.builder(
+            ? GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
+                itemBuilder: ((context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    child: GridTile(child: Image.network(item.image)),
+                  );
+                }),
                 itemCount: CatalogModel.items.length,
-                itemBuilder: (context, index) => ItemWidget(
-                  item: CatalogModel.items[index],
-                ),
               )
             : const Center(
                 child: CircularProgressIndicator(),
